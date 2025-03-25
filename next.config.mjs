9efd9cfb09a35687d6db59ac9bc6a182496281c7
y-azy <25907755+y-azy@users.noreply.github.com>
2025-03-25 16:42:03 -0400

    commit 2

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /framer-motion/,
+      sideEffects: false
+    });
+    return config;
+  },
   experimental: {},
   images: {
     remotePatterns: [],
